d79994e2ea15eee36f64c357c37be2bd9db1c5ae
codeisfound1 <c.odeisfound@gmail.com>
2026-04-24 23:28:27 +0700

    Change server port from 3001 to 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const shortid = require('shortid');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
